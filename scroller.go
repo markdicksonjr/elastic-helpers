b915f59bc3061580d41209dbd1357708c0375cf3
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-01-09 00:11:45 -0500

    Scroll helper added

---
+++ scroller.go
@@ -0,0 +1,59 @@
+package elastic_helpers
+
+import (
+	"context"
+	"errors"
+	"github.com/olivere/elastic"
+	"io"
+)
+
+type Scroller struct {
+	Client		*elastic.Client
+	Index 		string
+	Type 		string
+	Query		elastic.Query
+	Size		int
+
+	scrollId	string
+}
+
+func (s *Scroller) Continuous(
+	onBatch func(result *elastic.SearchResult) error,
+	onComplete func() error,
+) error {
+	res, err := s.Client.Scroll(s.Index).Type(s.Type).Query(s.Query).Size(s.Size).Do(context.TODO())
+	if err != nil {
+		return err
+	}
+	if res == nil {
+		return errors.New("expected results != nil; got nil")
+	}
+	if res.ScrollId == "" {
+		return errors.New("expected scrollId in results; got \"\"")
+	}
+
+	err = onBatch(res)
+
+	complete := false
+	for !complete {
+		res, err := s.Client.Scroll(s.Index).ScrollId(res.ScrollId).Size(s.Size).Do(context.TODO())
+
+		if err == io.EOF {
+			complete = true
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if res.Hits == nil || len(res.Hits.Hits) == 0 {
+			complete = true
+		}
+
+		err = onBatch(res)
+	}
+
+	return onComplete()
+}
+
