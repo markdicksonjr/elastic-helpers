8f457f69c30cb52b37ce1cdbd97382409adaef58
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-01-09 00:42:53 -0500

    GetAll function added
    Not sure this is a great thing to do, but here it is

---
+++ operations/get.go
@@ -0,0 +1,46 @@
+package operations
+
+import (
+	"github.com/markdicksonjr/elastic-helpers"
+	"github.com/markdicksonjr/elastic-helpers/formats"
+	"github.com/olivere/elastic"
+)
+
+func GetAll(
+	client *elastic.Client,
+	indexValue string,
+	typeValue string,
+	query elastic.Query,
+) ([]map[string]interface{}, error) {
+	scrollerInstance := elastic_helpers.Scroller{
+		Index: indexValue,
+		Type: typeValue,
+		Size: 1000,
+		Client: client,
+		Query: query,
+	}
+
+	var finalResults []map[string]interface{}
+	var asyncError error
+	complete := make(chan bool)
+	go func() {
+		asyncError = scrollerInstance.Continuous(func(result *elastic.SearchResult) error {
+			results, err := formats.UnmarshalSearchResult(result)
+			if err != nil {
+				return err
+			}
+
+			finalResults = append(finalResults, results...)
+
+			return nil
+		}, func() error {
+			complete <- true
+			return nil
+		})
+	}()
+
+	// wait until the complete channel is written to
+	<- complete
+
+	return finalResults, asyncError
+}
