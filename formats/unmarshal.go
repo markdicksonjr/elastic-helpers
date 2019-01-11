761e4ef4388ec062a710e758e2058205a2a042fd
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-01-10 23:53:30 -0500

    More generic unmarshal added

--- formats/unmarshal.go
+++ formats/unmarshal.go
@@ -3,6 +3,7 @@ package formats
 import (
 	"encoding/json"
 	"github.com/olivere/elastic"
+	"reflect"
 )
 
 type ConvertJsonFn = func(*json.RawMessage) (interface{}, error)
@@ -13,6 +14,13 @@ func UnmarshalRawJsonToMap(source *json.RawMessage) (map[string]interface{}, err
 		return nil, err
 	}
 	return item.(map[string]interface{}), nil
+}
+
+// typeOfPtr should be the type of a pointer to the type you're unmarshalling to
+// e.g. "reflect.TypeOf(&Score{})"
+func UnmarshalJson(jsonString *json.RawMessage, typeOfPtr reflect.Type) (interface{}, error) {
+	obj := reflect.New(typeOfPtr.Elem()).Interface()
+	return obj, json.Unmarshal(*jsonString, obj)
 }
 
 func UnmarshalSearchResultToMap(result *elastic.SearchResult) ([]map[string]interface{}, error) {
@@ -41,4 +49,18 @@ func UnmarshalSearchResult(result *elastic.SearchResult, convertFn ConvertJsonFn
 	}
 
 	return unmarshalledResults, nil
+}
+
+func UnmarshalSearchResultToType(result *elastic.SearchResult, typeOfPtr reflect.Type) ([]interface{}, error) {
+	results := make([]interface{}, len(result.Hits.Hits))
+	var err error
+
+	for index, hit := range result.Hits.Hits {
+		results[index], err = UnmarshalJson(hit.Source, typeOfPtr)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return results, nil
 }
