0f8c041b2f8dc7b7b3f475623908461b8a158f34
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-05-05 23:22:10 -0400

    Removed . from unsafe props
    Use dot or similar lib to set structs

--- formats/property.go
+++ formats/property.go
@@ -3,9 +3,8 @@ package formats
 import "strings"
 
 func EncodePropertyName(potentiallyUnsafeName string) string {
-	return strings.Replace(strings.Replace(strings.Replace(
+	return strings.Replace(strings.Replace(
 		potentiallyUnsafeName,
 		"(", "_", -1),
-		")", "_", -1),
-		".", "_", -1)
+		")", "_", -1)
 }
