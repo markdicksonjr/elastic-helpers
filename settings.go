d9f352e3d7d1136d9b2650370d67957429730fa9
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-06-02 14:54:52 -0400

    MaxResultWindow setter added

---
+++ settings.go
@@ -0,0 +1,14 @@
+package elastic_helpers
+
+import (
+	"context"
+	"github.com/olivere/elastic"
+	"strconv"
+)
+
+func SetMaxResultWindow(client *elastic.Client, index string, windowSize int) (acked bool, err error) {
+	jsonBody := "{ \"index\" : { \"max_result_window\" : " + strconv.Itoa(windowSize) + " } }"
+	settingsRes, err := client.IndexPutSettings(index).BodyJson(jsonBody).Do(context.TODO())
+	acked = settingsRes != nil && settingsRes.Acknowledged
+	return
+}
