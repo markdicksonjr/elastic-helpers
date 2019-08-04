d988a30e60fa56be589e0ef8ef9d57282f180503
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-08-04 19:03:24 -0400

    DB-wide max settings window update

--- settings.go
+++ settings.go
@@ -6,7 +6,14 @@ import (
 	"strconv"
 )
 
-func SetMaxResultWindow(client *elastic.Client, index string, windowSize int) (acked bool, err error) {
+func SetMaxResultWindow(client *elastic.Client, windowSize int) (acked bool, err error) {
+	jsonBody := "{ \"index\" : { \"max_result_window\" : " + strconv.Itoa(windowSize) + " } }"
+	settingsRes, err := client.IndexPutSettings().BodyJson(jsonBody).Do(context.TODO())
+	acked = settingsRes != nil && settingsRes.Acknowledged
+	return
+}
+
+func SetMaxResultWindowForIndex(client *elastic.Client, index string, windowSize int) (acked bool, err error) {
 	jsonBody := "{ \"index\" : { \"max_result_window\" : " + strconv.Itoa(windowSize) + " } }"
 	settingsRes, err := client.IndexPutSettings(index).BodyJson(jsonBody).Do(context.TODO())
 	acked = settingsRes != nil && settingsRes.Acknowledged
