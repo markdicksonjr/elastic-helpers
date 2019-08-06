b737afd5c498ab99336001679d02315a8de1b5b0
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-08-06 15:41:21 -0400

    Trim and trailing . added for property name encoding

---
+++ formats/property_test.go
@@ -0,0 +1,25 @@
+package formats
+
+import "testing"
+
+func TestEncodePropertyName(t *testing.T) {
+	propRes := EncodePropertyName("prop.X.")
+	if propRes != "prop.X" {
+		t.Fatal("trailing period was not removed")
+	}
+
+	propReses := EncodePropertyName("prop.X..")
+	if propReses != "prop.X" {
+		t.Fatal("trailing periods were not removed")
+	}
+
+	propParents := EncodePropertyName("Product (FG)")
+	if propParents != "Product _FG_" {
+		t.Fatal("parentheses were not removed")
+	}
+
+	propTrailingSpace := EncodePropertyName("Trailing ")
+	if propTrailingSpace != "Trailing" {
+		t.Fatal("trailing space was not removed")
+	}
+}
